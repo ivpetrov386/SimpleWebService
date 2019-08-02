733e6aae2fc493f5ed32acf879996af68ea84431
Ivan Petrov <ipetrov@idscan.net>
2019-08-02 15:08:54 +0500

    initial commit

--- SimpleWebService/Controllers/ValuesController.cs
+++ SimpleWebService/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace SimpleWebService.Controllers
 {
@@ -10,11 +11,17 @@ namespace SimpleWebService.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return _configuration["id"];
         }
 
         // GET api/values/5
